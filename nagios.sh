9c27aca09e71c2b03a31a935c7e020948c43f94d
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2020-04-21 18:31:17 -0700

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -1,103 +1,51 @@
 #!/bin/bash
 
-yum -y install nagios  #Install, start, and enable nagios
+yum -y install nagios           #install, start and enable nagios
 systemctl enable nagios
 systemctl start nagios
 
-setenforce 0  #Turn off SElinux so it doesn't trip on us
+setenforce 0                    # turn off SELinux, so it doesn't trip us up
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config  #disable it perminatly
 
-systemctl enable httpd  # Enable and start Apache
+systemctl enable httpd          # Enable and start apache
 systemctl start httpd
 
-yum -y install nrpe  #Install, enable, and start nrpe, the nagios client
+yum -y install nrpe             #install, enable, and start nrpe, the nagios client
 systemctl enable nrpe
 systemctl start nrpe
 yum -y install nagios-plugins-all
 yum -y install nagios-plugins-nrpe
 
-htpasswd -b /etc/nagios/passwd nagiosadmin nagiosadmin  #set the nagios admin password
-sed -i 's,allowed_hosts=127.0.0.1,allowed_hosts=127.0.0.1\, 10.128.0.46\/20,g' /etc/nagios/nrpe.config
-#above enables connections from your subnet, need to adjust subnet
+htpasswd -b /etc/nagios/passwd nagiosadmin nagiosadmin      #set the nagios admin password
+sed -i 's,allowed_hosts=127.0.0.1,allowed_hosts=127.0.0.1\, 10.128.0.8\/20, g' /etc/nagios/nrpe.cfg
+#enables connections from the subnet please adjust to your subnet.
 
-sed -i 's,dont_blame_nrpe=0,dont_blame_nrpe=1,g /etc/nagios.cfg
-#enable NRPE monitoring
+sed -i 's,dont_blame_nrpe=0,dont_blame_nrpe=1,g' /etc/nagios/nrpe.cfg
+#enables NRPE monitoring
 
 mkdir /etc/nagios/servers
 #create a directory for our server configuration and enable it in the config file
-sed -i 's,#cfg_dir=/etc/nagios/servers/,cfg_dir=/etc/nagios/servers,g' /etc/nagios/nagios.cfg
+sed -i 's,#cfg_dir=/etc/nagios/servers,cfg_dir=/etc/nagios/servers,g' /etc/nagios/nagios.cfg
+echo 'define command{
+                          command_name check_nrpe
+                          command_line /usr/lib64/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$
+                          }' >> /etc/nagios/objects/commands.cfg
+                          
 systemctl restart nagios
 
-cd /etc/nagios/servers
+yum -y install wget
+cd /etc/nagios
+wget https://raw.githubusercontent.com/nic-instruction/hello-nti-320/master/generate_config.sh
 
-#Spin up a machine called example-a with all the nrpe plugins installed and properly configured path to
-#nagios
-#put its ip address in the address column below
 
-echo '# Define a host for an example-a machine
-define host{
-  use                   linux-server
-  host_name             example-a
-  alias                 example-a
-  address               put address here
-  }
-###############################################################################################
-###############################################################################################
-#
-# SERVICE DEFINITIONS
-#
-################################################################################################
-################################################################################################
-#Define a service to ping the example-a machine
-define service{
-      use                   generic-service                   ; Name of service template to use
-      host_name             example-a                         
-      service_description   PING
-      check_command         check_ping!100.0,20%!500.0,60%
-      }
- #Define a service to check HTTP on the example-a machine.
- #Disable notifications for this service by default, as not all users may have HTTP enabled.
- define service{
-          use                             generic-service         ; Name of service template to use
-          host_name                       example-a
-          service_description             HTTP 
-          chec_command                    check_http
-          notifications_enabled           0
-          }
-#Define a service to check the disk space of the root partition
-#on the example-a machine. Warning if < 20% free, critical
-# < 10% free space on partition
-define service{
-       use                    generic-service                   ; Name of service template to use
-       host_name              example-a
-       service_description    Root Partition
-       check_command                    check_nrpe!check_disk!20%!10%!/
-       }
-#Define a service to check the number of currently logged in
-#users on the example-a machine. Warning if > 20 users, critical if
-#> 50 users.
-define service{
-        use                   generic-service                   ; Name of service template to use
-        host_name             example-a
-        service_description   Current Users
-        check_command                       check_nrpe!check_users!20!50
-        }
-#Define a service to check the number of currently running processes
-#on the example-a machine. Warning if > 250 processes, critical if
-# > 400 processes
-define service{
-               use                    generic-service                   ; Name of service template to use
-               host-name              example-a
-               service_description    Total Processes
-               check_command                        check_nrpe!check_procs!250!400!RSZDT
-               }
-#Define a service to check the load on the example-a machine             
-define service{
-               use                    generic-service                   ; Name of service template to use
-               host-name              example-a
-               service_description    Current Load
-               check_command                        check_nrpe!check_load!5.0,4.0,3.0!10.0,6.0,4.0
-               }
-#Define a service to check SSH on the example-a machine
-#Disablesi
- 
- 
+
+# Now take a break, and spin up a machine called example-a with all the nrpe plugins installed and a propperly configured path 
+# to nagios
+# put it’s ip address and hostname insto generate_config.sh
+
+#Further configuration:
+#https://assets.nagios.eom/downloads/nagioscore/docs/nagioscore/4/en/monitoring-publicservices.html (Links to an external site.}
+
+#verify
+#/usr/sbin/nagios -v /etc/nagios/nagios.cfg
+
