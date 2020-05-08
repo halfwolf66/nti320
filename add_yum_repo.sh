c1548862f5a13bc995d6defb75bc563daae8fd87
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2020-05-07 19:37:16 -0700

    Update add_yum_repo.sh

--- add_yum_repo.sh
+++ add_yum_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this is a script to add a yum repo on a given server.
 
-ip="10.128.0.13"                                                    # Please replace with your own IP
+ip="10.128.0.53"                                                    # Please replace with your own IP
 
 echo "[nti-320]
 name=Extra Packages for Centos from NTI-320 7 - $basearch
