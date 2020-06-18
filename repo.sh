3ac65b6ea535bce8a91432240af59db5cd85a326
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2020-06-17 19:49:01 -0700

    Create repo.sh

---
+++ repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# this is a script to add a yum repo on a given server.
+ip="10.128.0.53"                                                    # Please replace with your own IP
+echo "[nti-320]
+name=Extra Packages for Centos from NTI-320 7 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
+# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
+baseurl=http://$ip/centos/7/extras/x86_64/Packages/
+enabled=1
+gpgcheck=0
+" >> /etc/yum.repos.d/NTI-320.repo
