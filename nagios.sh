e7d5d8a7030fb11437a3fb0d3926115ee63f821e
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2020-06-18 12:08:38 -0700

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
+echo "[nti-310-epel]
+name=NTI310 EPEL
+baseurl=http://104.197.59.12/epel
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-base]
+name=NTI310 BASE
+baseurl=http://104.197.59.12/base
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-320]
+name=Extra Packages for Centos from NTI-320 7 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
+# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
+baseurl=http://10.128.0.53/centos/7/extras/x86_64/Packages/
+enabled=1
+gpgcheck=0
+" > /etc/yum.repos.d/NTI-320.repo
 
 yum -y install nagios
 systemctl enable nagios
