21bc99d894340be1cb182d422edafaabfc8663bf
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2020-06-18 19:17:03 -0700

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -20,12 +20,7 @@ name=NTI310 UPDATES
 baseurl=http://104.197.59.12/updates/
 gpgcheck=0
 enabled=1" >> /etc/yum.repos.d/local-repo.repo
-#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
-# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
-baseurl=http://10.128.0.53/centos/7/extras/x86_64/Packages/
-enabled=1
-gpgcheck=0
-" > /etc/yum.repos.d/NTI-320.repo
+
 
 yum -y install nagios
 systemctl enable nagios
